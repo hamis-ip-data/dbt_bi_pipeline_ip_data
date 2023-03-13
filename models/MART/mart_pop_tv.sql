6c314523942eb027d22bdd063d8c0593e66fce73
Equipe Dache <equipe.dache@gmail.com>
2023-03-13 19:31:27 +0000

    update data  pipeline

---
+++ models/MART/mart_pop_tv.sql
@@ -0,0 +1,47 @@
+  
+
+  {{
+    config(
+      materialized = 'table'
+      )
+  }}
+
+  select
+    distinct 
+    day,
+    campaign_name,
+    media_buy_key,
+    media_buy_position,
+    media_buy_type,
+    media_buy_targeting_age,
+    horaire_hh_mm,
+    emission_avant,
+    emission_apres,
+    libelle_client,
+    dict.final_advertiser_name,
+    dict.id_media_manager,
+    dict.id_nouveau,
+    dict.nom_annonceur,
+    dict.ministere_annonceur,    
+    site_name,
+    creative_name,
+    site_category,
+    site_network,
+    grp,
+    position,
+    tarif_brut_pay,
+    tarif_bt_amp,
+    tarif_net_co,
+    tarif_bt_fact,
+    tarif_net_fo,
+    tarif_net_fo_hi,
+    tarif_secodip,
+    duree_de_spot,
+    grp_pc_actifs,
+    grp_pc_enf4a10,
+    grp_pc_ens1219,
+    grp_pc_ens8_12,
+
+from {{ ref('prd_pop_tv') }} as prod_pop 
+left join {{ ref('prd_advertiser_dict') }} dict 
+on prod_pop.libelle_client = dict.advertiser
